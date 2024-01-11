041734490c7f2127b805f681099cfc5e607372c3
Kaan <kaan.kalmioglu@hva.nl>
2024-01-11 11:51:42 +0100

    removing unneeded comments
    removed comments in code, since i have written these down in my own document where i keep track of everything new i learn/use

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 updateSlide(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   updateSlide(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   updateSlide(slideIndex = n);
 }
@@ -15,14 +13,11 @@ function updateSlide(n) {
   let slides = [...document.getElementsByClassName("carousel")];
   let dots = [...document.getElementsByClassName("dot")];
 
-  // Adjust slideIndex if it's out of bounds
   slideIndex = ((n - 1 + slides.length) % slides.length) + 1;
 
-  // Hide all slides and deactivate all dots
   slides.forEach(slide => slide.style.display = "none");
   dots.forEach(dot => dot.className = dot.className.replace(" active", ""));
 
-  // Show the current slide and activate the corresponding dot
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
