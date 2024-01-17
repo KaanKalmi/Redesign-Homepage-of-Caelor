bdb9bfaf86e944c6651d2b2f209a131a8a8454f3
Kaan <kaan.kalmioglu@hva.nl>
2024-01-17 23:54:04 +0100

    It took me so long... and it works 50%
    Had no idea JS would be THIS hard, i used up all of my 3 lifelines (we all love a little bit of we wants to be a millionaire?)

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,50 @@
-/*Slideshow JS moet verandert worden, geen JS in HTML, interactie voor extra info moet erbij komen, event listeners etc de 3 stappenplan dus*/
-let slideIndex = 1;
-updateSlide(slideIndex);
+let currentSlideIndex = 1; //Hier declare ik de variabele currentSlideIndex en geef ik hem de waarde 1
+updateSlideDisplay(currentSlideIndex); //Roept de functie updateSlideDisplay aan met de currentslideIndex als argument
 
-function plusSlides(n) {
-  updateSlide(slideIndex += n);
+function changeSlide(n) { 
+  updateSlideDisplay(currentSlideIndex += n); //Geeft de currentSlideIndex een +1 of -1
 }
 
-function currentSlide(n) {
-  updateSlide(slideIndex = n);
+function goToSlide(n) { 
+  updateSlideDisplay(currentSlideIndex = n); //Definieert de updateSlideDisplay functie met de n als argument en roept vervolgens de nieuwe currentSlideIndex op
 }
 
-function updateSlide(n) {
-  let slides = [...document.getElementsByClassName("carousel")];
-  let dots = [...document.getElementsByClassName("dot")];
+function updateSlideDisplay(n) { //Definieert de updateSlideDisplay functie met de n als argument
+  let slideElements = [...document.getElementsByClassName("carousel")]; //Definieert de slideElements variabele en assigned hem aan alle elementen met de carousel class
+  let dotElements = [...document.getElementsByClassName("dot")]; //Definieert de dotElements variabele en assigned hem aan alle elementen met de dot class
+  let articleElements = [...document.getElementsByClassName("content")]; //Definieert de articleElements variabele en assigned hem aan alle elementen met de content class
 
-  slideIndex = ((n - 1 + slides.length) % slides.length) + 1;
+  currentSlideIndex = ((n - 1 + slideElements.length) % slideElements.length) + 1; 
+  /* De lijn hierboven berekent de nieuwe currentSlideIndex in een circulaire manier, de + slideElements,length zorgt ervoor dat de
+  index niet negatief kan worden, als n<1 is dan wrapped hij naar de start, of als n>slideElements.length dan wrapped hij ook*/
+  slideElements.forEach(slide => slide.style.display = "none"); //Hier wordt de display van alle slides op none gezet
+  dotElements.forEach(dot => dot.className = dot.className.replace(" active", "")); //Hier wordt de active class van alle dots verwijderd
+  articleElements.forEach(article => article.style.display = "none"); //Hier wordt de display van alle articles op none gezet
 
-  slides.forEach(slide => slide.style.display = "none");
-  dots.forEach(dot => dot.className = dot.className.replace(" active", ""));
-
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slideElements[currentSlideIndex-1].style.display = "block"; //Hier wordt de display van de slide met de nieuwe currentSlideIndex op block gezet
+  dotElements[currentSlideIndex-1].className += " active"; //Hier wordt de active class aan de dot met de nieuwe currentSlideIndex toegevoegd
+  articleElements[currentSlideIndex-1].style.display = "block"; //Hier wordt de display van de article met de nieuwe currentSlideIndex op block gezet
 }
 
-//doe een event listener op de button en dan op de <article> een class togglen met display:block en display:none
-// inline JS veranderen naar event listeners hiero
+document.addEventListener('DOMContentLoaded', (event) => { //Hier wordt de DOMContentLoaded eventlistener aangemaakt
+  let previousButton = document.querySelector(".prev"); //Hier wordt de previousButton variabele aangemaakt en assigned aan het element met de prev class
+  let nextSlideButton = document.querySelector(".next"); //Hier wordt de nextSlideButton variabele aangemaakt en assigned aan het element met de next class
+  let dotElements = [...document.querySelectorAll(".dot")]; //Hier wordt de dotElements variabele aangemaakt en assigned aan alle elementen met de dot class
+
+  previousButton.addEventListener("click", function() { changeSlide(-1); }); //Hier wordt de click eventlistener aangemaakt voor de previousButton en verandert de currentslide met -1
+  nextSlideButton.addEventListener("click", function() { changeSlide(1); }); //Hier wordt de click eventlistener aangemaakt voor de nextSlideButton en verandert de currentslide met +1
+
+  dotElements.forEach((dot, index) => { //Hier wordt er een eventlistener aangemaakt voor elke dot en verandert de currentslide naar de index van de dot
+      dot.addEventListener("click", function() { goToSlide(index + 1); }); //Hier wordt de click eventlistener aangemaakt voor de dot en verandert de currentslide naar de index van de dot
+  });
+});
+
+// Hieronder staan alle console.logs om te testen of alles werkt
+console.log(currentSlideIndex);
+console.log(slideElements);
+console.log(dotElements);
+console.log(articleElements);
+console.log(previousButton);
+console.log(nextSlideButton);
+console.log(dotElements);
+console.log(dotElements);
