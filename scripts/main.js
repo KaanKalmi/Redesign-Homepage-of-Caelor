5a01000ffe463e58d5d630f68d0c8c4ec6fff7d7
Kaan <kaan.kalmioglu@hva.nl>
2024-01-24 19:33:59 +0100

    biggest update that i have ever dropped

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@ function goToSlide(n) {
   showSlide();
 }
 
-window.onload = function() {
+window.addEventListener('load', function() {
   let previousButton = document.querySelector(".prev");
   let nextSlideButton = document.querySelector(".next");
 
@@ -41,4 +41,49 @@ window.onload = function() {
   };
 
   showSlide();
+});
+
+let currentSlideIndex2 = 0;
+let slides2 = document.getElementsByClassName("carousel-item-2");
+let dots2 = document.getElementsByClassName("dot-2");
+
+function showSlide2() {
+  for (let i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (let i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[currentSlideIndex2].style.display = "block";
+  dots2[currentSlideIndex2].className += " active";
+}
+
+function changeSlide2(n) {
+  currentSlideIndex2 += n;
+  if (currentSlideIndex2 >= slides2.length) {
+    currentSlideIndex2 = 0;
+  }
+  if (currentSlideIndex2 < 0) {
+    currentSlideIndex2 = slides2.length - 1;
+  }
+  showSlide2();
+}
+
+function goToSlide2(n) {
+  currentSlideIndex2 = n;
+  showSlide2();
+}
+
+window.onload = function() {
+  let previousButton2 = document.querySelector(".prev-2");
+  let nextSlideButton2 = document.querySelector(".next-2");
+
+  previousButton2.onclick = function() {
+    changeSlide2(-1);
+  };
+  nextSlideButton2.onclick = function() {
+    changeSlide2(1);
+  };
+
+  showSlide2();
 };
